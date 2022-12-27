5fc5c176fff2a11c5ff497f6bf679b3723411d85
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-12-27 22:40:29 +0300

    Added title and changed icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { GlobalStyles } from '@components/global_styles'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import 'styles/reset.css'
 import { Layout } from '../components/layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>YAZMEYAA</title>
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </Layout>
