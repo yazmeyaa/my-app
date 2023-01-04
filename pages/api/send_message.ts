39c708795fa34aa8ef3d6d5f206518b26037d32b
yazmeyaa <vip.tonuvmartini@mail.ru>
2023-01-04 23:19:01 +0300

    CORS fix

--- pages/api/send_message.ts
+++ pages/api/send_message.ts
@@ -13,6 +13,15 @@ interface RequestWithBody extends NextApiRequest {
 }
 
 export default async function getWeatherByCityName(req: RequestWithBody, res: NextApiResponse) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    )
+    if (req.method == "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
     const API_KEY = process.env.TELEGRAM_BOT_API_KEY
     const myChatId = 279603779
     if (!API_KEY) throw new Error('NO API KEY PROVIDED')
