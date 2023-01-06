330f3cd82a963f1c4bdc383bfa826522647d4c58
yazmeyaa <vip.tonuvmartini@mail.ru>
2023-01-06 14:28:04 +0300

    fixed sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,22 +1,13 @@
 import { GetServerSideProps } from "next"
 
 const Sitemap = () => {
-    return null
+  return null
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-    const BASE_URL = 'http://localhost:3000'
 
-    const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-    
-    
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>https://yazmeyaa.itracers.xyz/</loc>
       <lastmod>2023-01-05T18:03:31+00:00</lastmod>
@@ -37,18 +28,16 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
       <lastmod>2023-01-02T20:19:52+00:00</lastmod>
       <priority>0.80</priority>
     </url>
-    
-    
     </urlset>
   `
 
-    res.setHeader('Content-Type', 'text/xml')
-    res.write(sitemap)
-    res.end()
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
 
-    return {
-        props: {}
-    }
+  return {
+    props: {}
+  }
 }
 
 export default Sitemap
